709019ea6b2154845fcd91242df4f52b25a1073c
Мадияр Асқарұлы <madiaraskarly@Madiars-MacBook-Air.local>
2025-02-16 21:01:17 +0500

    Remove files now ignored by .gitignore

--- backend/index.js
+++ backend/index.js
@@ -188,7 +188,7 @@ app.delete("/assessment/:userId", async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5555;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
